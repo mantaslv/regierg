40d85eb8d173895eaa3c38af2af54ffffa3ed03f
mantaslv <mantasvolkas@gmail.com>
2024-06-20 13:25:19 +0100

    Parse result returned as JSON

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from datetime import datetime
+import json
 
 class ErgSession:
 	def __init__(self):
 		self.monitor_model = None
+	
+	def to_json(self):
+		return json.dumps({
+			"monitor_model": self.monitor_model
+		}, indent=4)
     
 def parse_erg_data(data):
 	session = ErgSession()
@@ -15,5 +21,5 @@ def parse_erg_data(data):
 
 data = ['Oconcept 2', 'PM5', 'View Detail', '5x6:00/2:00r', 'Total Time:', 'Apr 18 2024', '40:00.0', 'time meter', '1500m', '30:00.0', '8482', '1:46.1 21', '6:00.0', '1627', '1:50.6 20 150', '6:00.0', '1677 1:47.3 21 161', '6:00.0', '1736', '1:43.6 23 169', '6:00.0', '1718. 1:44.7 22 171', '6:00.0', '1723 1:44.4 22 180', 'r245', 'Units', 'Display', 'Menu']
 session = parse_erg_data(data)
-
-print(session.monitor_model)
+json_output = session.to_json()
+print(json_output)
