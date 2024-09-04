d17cbd0c0d557679edda027c650ff38a827e073f
mantaslv <mantasvolkas@gmail.com>
2024-09-04 20:07:35 +0100

    Session name if failed parse works out from intervals

--- services/erg_parser.py
+++ services/erg_parser.py
@@ -165,4 +165,60 @@ def parse_erg_data(data):
         # Add the interval to the session
         session.add_interval(duration, meters, split_time, stroke_rate, heart_rate)
 
+    time_format = "%H:%M:%S.%f"
+
+    if session.total_time is not None and session.total_time < session.row_time:
+        incorrect_total_time_str = session.total_time.strftime(time_format)
+        row_time_str = session.row_time.strftime(time_format)
+
+        incorrect_total_time_list = list(incorrect_total_time_str)
+
+        for index, char in enumerate(incorrect_total_time_list):
+            if char == "2" and row_time_str[index] == "3":
+                incorrect_total_time_list[index] = "3" 
+
+        corrected_total_time_str = ''.join(incorrect_total_time_list)
+
+        session.total_time = datetime.strptime(corrected_total_time_str, time_format).time()
+
+    if session.session_name is None:
+        number_of_intervals = len(session.intervals)
+
+        if session.total_time > session.row_time:
+            total_rest = subtract_times(session.total_time, session.row_time)
+            rest = remove_leading_zeros_from_time(str(total_rest / number_of_intervals))
+            interval = None
+            
+            if session.intervals[0]["duration"] == session.intervals[1]["duration"]:
+                interval = session.intervals[0]["duration"]
+            else:
+                interval = str(session.intervals[0]["meters"]) + "m"
+
+            session.session_name = f"{number_of_intervals}x{interval}/{rest}r"
+        else:
+            if session.intervals[-1]["meters"] / number_of_intervals == session.intervals[0]["meters"]:
+                session.session_name = session.meters + "m"
+            else:
+                session_name = remove_leading_zeros_from_time(str(session.row_time))
+                if len(session_name.split(":")[-1]) == 1:
+                    session_name += "0"
+                session.session_name = session_name
     return session
+
+def subtract_times(time1, time2):
+    datetime1 = datetime.combine(datetime.today(), time1)
+    datetime2 = datetime.combine(datetime.today(), time2)
+    return datetime1 - datetime2
+
+def remove_leading_zeros_from_time(time):
+    time_parts = time.split(':')
+
+    if time_parts[0] == "0" or time_parts[0] == "00":
+        time_parts.pop(0)
+        
+        if time_parts[0] == "00":
+            time_parts.pop(0)
+    
+    cleaned_time_parts = [str(int(part)) for part in time_parts]
+    cleaned_time_str = ':'.join(cleaned_time_parts)
+    return cleaned_time_str
